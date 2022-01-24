ace9d7945357bcb50c4aa18c657913fe25cc3c42
Edison Mera <efmera@gmail.com>
2022-01-24 23:32:38 +0100

    Added support for deployment of a pxe server

--- deploy.sh
+++ deploy.sh
@@ -23,11 +23,11 @@ set -e
 # Machine specific configuration:
 USERNAME=admin
 FULLNAME="Administrative Account"
-HOSTNAME=pxeclient
+DESTNAME=debian1
 # Specifies if the machine is encrypted:
 # ENCRYPT=yes
 # TANG Server, leave it empty to disable:
-#TANGSERV=10.8.0.2
+TANGSERV=10.8.0.2
 # Use TPM2, if available
 if [ -e /dev/tpm0 ]; then
     WITHTPM2=1
@@ -35,9 +35,9 @@ else
     WITHTPM2=0
 fi
 # Extra packages you want to install, leave empty for a small footprint
-# Equivalent to live xfce4 installation + some tools
 DEBPACKS="acl binutils build-essential openssh-server"
 # DEBPACKS+=" emacs firefox-esr"
+# Equivalent to live xfce4 installation + some tools
 # DEBPACKS+=" xfce4 task-xfce-desktop"
 # DEBPACKS+=" lxde task-lxde-desktop"
 # DEBPACKS+=" cinnamon task-cinnamon-desktop"
@@ -52,7 +52,11 @@ DISKLAYOUT=singboot
 # DISK=/dev/nvme0n1
 DISK=/dev/vda
 
+ROOTDIR=${ROOTDIR:-/mnt}
+
 LASTCHDSK=${DISK: -1}
+
+ROOTMODE=ro
 
 if [ "${LASTCHDSK##[0-9]}" == "" ] ; then
     PSEP="p"
@@ -94,8 +98,8 @@ warn_confirm () {
 
 config_key () {
     echo "The master passphrase is used as failover decryption method and admin user password"
-    export KEY_="$($ASKPASS_ "Master passphrase:")"
-    CONFIRM_="$($ASKPASS_ "Confirm master passphrase:")"
+    export KEY_="$($ASKPASS_ "New password:")"
+    CONFIRM_="$($ASKPASS_ "Retype new password:")"
     if [ "${KEY_}" != "${CONFIRM_}" ] ; then
         echo "ERROR: Confirmation didn't match, aborting"
         exit 1
@@ -140,23 +144,23 @@ build_partitions () {
         mkdosfs -F 32 -s 1 -n EFI ${PARTUEFI}
     fi
     
-    mount $ROOTPART /mnt
-    btrfs subvolume create /mnt/@
-    mkdir /mnt/@/home
-    mkdir /mnt/@/boot
-    btrfs subvolume create /mnt/@home
+    mount $ROOTPART ${ROOTDIR}
+    btrfs subvolume create ${ROOTDIR}/@
+    mkdir ${ROOTDIR}/@/home
+    mkdir ${ROOTDIR}/@/boot
+    btrfs subvolume create ${ROOTDIR}/@home
 
     if [ "$ENCRYPT" == yes ] ; then
-        dd if=/dev/urandom bs=2048 count=1 of=/mnt/@/crypto_keyfile.bin
-        chmod go-rw /mnt/@/crypto_keyfile.bin
-        printf "%s" "$KEY_"|cryptsetup luksAddKey ${PARTROOT} /mnt/@/crypto_keyfile.bin --key-file -
-        printf "%s" "$KEY_"|cryptsetup luksAddKey ${PARTSWAP} /mnt/@/crypto_keyfile.bin --key-file -
+        dd if=/dev/urandom bs=2048 count=1 of=${ROOTDIR}/@/crypto_keyfile.bin
+        chmod go-rw ${ROOTDIR}/@/crypto_keyfile.bin
+        printf "%s" "$KEY_"|cryptsetup luksAddKey ${PARTROOT} ${ROOTDIR}/@/crypto_keyfile.bin --key-file -
+        printf "%s" "$KEY_"|cryptsetup luksAddKey ${PARTSWAP} ${ROOTDIR}/@/crypto_keyfile.bin --key-file -
     fi
 
-    umount /mnt
-    mount ${PARTBOOT} /mnt
-    mkdir /mnt/efi
-    umount /mnt
+    umount ${ROOTDIR}
+    mount ${PARTBOOT} ${ROOTDIR}
+    mkdir ${ROOTDIR}/efi
+    umount ${ROOTDIR}
 }
 
 setup_aptinstall () {
@@ -167,7 +171,7 @@ setup_aptinstall () {
 }
 
 setup_apt () {
-    cat <<'EOF' > /mnt/etc/apt/sources.list
+    cat <<'EOF' > ${ROOTDIR}/etc/apt/sources.list
 deb http://deb.debian.org/debian bullseye main contrib
 deb-src http://deb.debian.org/debian bullseye main contrib
 
@@ -177,7 +181,7 @@ deb-src http://security.debian.org/debian-security bullseye-security main contri
 deb http://deb.debian.org/debian bullseye-updates main contrib
 deb-src http://deb.debian.org/debian bullseye-updates main contrib
 EOF
-    cat <<'EOF' > /mnt/etc/apt/sources.list.d/bullseye-backport.list
+    cat <<'EOF' > ${ROOTDIR}/etc/apt/sources.list.d/bullseye-backport.list
 deb http://deb.debian.org/debian bullseye-backports main contrib
 deb-src http://deb.debian.org/debian bullseye-backports main contrib
 EOF
@@ -191,37 +195,49 @@ setup_nic () {
           else
               echo "iface $nic inet loopback"
           fi
-        ) > /mnt/etc/network/interfaces.d/$nic
+        ) > ${ROOTDIR}/etc/network/interfaces.d/$nic
     done
 }
 
 setup_hostname () {
-    echo $HOSTNAME > /mnt/etc/hostname
+    echo $DESTNAME > ${ROOTDIR}/etc/hostname
     ( echo "127.0.0.1	localhost" ; \
       echo "::1		localhost ip6-localhost ip6-loopback" ; \
       echo "ff02::1		ip6-allnodes" ; \
       echo "ff02::2		ip6-allrouters" ; \
-      echo "127.0.1.1	$HOSTNAME" ; \
-      ) > /mnt/etc/hosts
+      echo "127.0.1.1	$DESTNAME" ; \
+      ) > ${ROOTDIR}/etc/hosts
 }
 
 unpack_debian () {
-    debootstrap bullseye /mnt
+    debootstrap bullseye ${ROOTDIR}
 }
 
 mount_partitions () {
-    mount $ROOTPART /mnt -o subvol=@
-    mount $ROOTPART /mnt/home -o subvol=@home
-    mount ${PARTBOOT} /mnt/boot
-    mount ${PARTUEFI} /mnt/boot/efi
+    mount $ROOTPART   ${ROOTDIR} -o subvol=@
+    mount $ROOTPART   ${ROOTDIR}/home -o subvol=@home
+    mount ${PARTBOOT} ${ROOTDIR}/boot
+    mount ${PARTUEFI} ${ROOTDIR}/boot/efi
 }
 
-rbind_systemdirs () {
-    mount --rbind /dev  /mnt/dev
-    mount --rbind /proc /mnt/proc
-    mount --rbind /sys  /mnt/sys
-    mount --rbind /run  /mnt/run
-    mount --rbind /tmp  /mnt/tmp
+bind_dirs () {
+    mount --bind /dev  ${ROOTDIR}/dev
+    mount --bind /proc ${ROOTDIR}/proc
+    mount --bind /sys  ${ROOTDIR}/sys
+    mount --bind /run  ${ROOTDIR}/run
+    mount --bind /tmp  ${ROOTDIR}/tmp
+    # mount --bind ${BASEDIR}/home ${ROOTDIR}/home
+    # mount --bind ${TFTPDIR}/boot ${ROOTDIR}/boot
+}
+
+unbind_dirs () {
+    umount -l ${ROOTDIR}/dev
+    umount -l ${ROOTDIR}/proc
+    umount -l ${ROOTDIR}/sys
+    umount -l ${ROOTDIR}/run
+    umount -l ${ROOTDIR}/tmp
+    # umount -l ${ROOTDIR}/home
+    # umount -l ${ROOTDIR}/boot
 }
 
 config_grubip () {
@@ -404,7 +420,7 @@ config_decrypt_clevis () {
     # that it will be safe to keep it in the initrd file.
 
     cat <<'EOF' | sed -e s:'<KEYSTORE>':"$KEYSTORE":g \
-                      -e s:'<HOSTNAME>':"$HOSTNAME":g \
+                      -e s:'<HOSTNAME>':"$DESTNAME":g \
                       > /lib/cryptsetup/scripts/decrypt_clevis
 #!/bin/sh
 
@@ -544,15 +560,16 @@ do_chroot () {
 }
 
 unmount_partitions () {
-    umount -l -R /mnt
+    umount -l -R ${ROOTDIR}
 }
 
 config_chroot () {
-    cp $0 /mnt/tmp/
-    chroot /mnt /tmp/deploy.sh do_chroot
+    cp $0 ${ROOTDIR}/tmp/
+    chroot ${ROOTDIR} /tmp/deploy.sh $*
 }
 
-all () {
+wipeout () {
+    ROOTDIR=/mnt
     warn_confirm
     config_key
     config_aptcacher
@@ -563,13 +580,203 @@ all () {
     setup_apt
     setup_hostname
     setup_nic
-    rbind_systemdirs
-    config_chroot
+    bind_dirs
+    config_chroot do_chroot
     unmount_partitions
 }
 
-if [ $# = 0 ] ; then
-    all | tee deploy.out
-else
-    $*
+config_fstab_pxe () {
+    ( echo "/dev/nfs                    /     nfs   tcp,nolock      0  0" ; \
+      echo "tmpfs                       /tmp  tmpfs nodev,nosuid    0  0" ; \
+      echo "ovrfs                       /etc  fuse  nofail,defaults 0  0" ; \
+      echo "ovrfs                       /var  fuse  nofail,defaults 0  0" ; \
+      echo "${SERVERIP}:${BASEDIR}/home /home nfs   rw,tcp,nolock   0  0" ; \
+      ) > ${ROOTDIR}/etc/fstab
+    mkdir -p ${ROOTDIR}${OVERDIR}/etc ${ROOTDIR}${OVERDIR}/var
+}
+
+do_chroot_pxe () {
+    config_aptcacher
+    config_initpacks
+    apt-get install --yes nfs-common fuse lsof
+    # apt-get --yes purge connman
+    # apt-get --yes autoremove
+    config_suspend
+    config_init
+    update-initramfs -c -k all
+}
+
+settings_pxe () {
+    DESTNAME=worker5
+    NADDRESS='10.8.0'
+    CLIENTIP=${NADDRESS}'.*'
+    DHCPIP=${NADDRESS}'.1'
+    SERVERIPS=`hostname -I`
+    SERVERIP=`( for i in ${SERVERIPS} ; do echo ${i} ; done ) | grep ${NADDRESS}`
+    BASEDIR=/srv/nfs4/pxe/${DESTNAME}
+    ROOTDIR=${BASEDIR}/root
+    if [ ! -f /etc/default/tftpd-hpa ] ; then
+        apt install tftpd-hpa
+    fi
+    TFTPROOT=`grep TFTP_DIRECTORY /etc/default/tftpd-hpa|sed 's/TFTP_DIRECTORY=\"\(.*\)\"/\1/g'`
+    TFTPDIR=${TFTPROOT}/${DESTNAME}
+}
+
+pxeserver () {
+    settings_pxe
+    if [ ! -d ${ROOTDIR} ] ; then
+        config_key
+        mkdir -p ${ROOTDIR} ${BASEDIR}/home ${TFTPDIR}/boot
+        unpack_debian
+        setup_apt
+        bind_dirs
+        config_chroot do_chroot_pxe
+        unbind_dirs
+    fi
+    config_fstab_d
+    config_fstab_pxe
+    config_overlay
+    setup_pxe
+    rm -f ${ROOTDIR}/etc/hostname ${ROOTDIR}/etc/hosts ${ROOTDIR}/etc/network/interfaces.d/*
+    mount -a -T /etc/fstab.d
+    systemctl restart nfs-kernel-server
+}
+
+setup_pxe () {
+    mkdir -p /etc/fstab.d /etc/exports.d \
+          ${TFTPDIR}/boot \
+          ${TFTPDIR}/pxelinux.cfg
+    
+    ln -f /usr/lib/PXELINUX/pxelinux.0               ${TFTPDIR}/pxelinux.0
+    ln -f /usr/lib/syslinux/modules/bios/ldlinux.c32 ${TFTPDIR}/ldlinux.c32
+    cp -d ${ROOTDIR}/vmlinuz                         ${TFTPDIR}/
+    cp -d ${ROOTDIR}/initrd.img                      ${TFTPDIR}/
+    
+    ( echo "${ROOTDIR} ${CLIENTIP}(${ROOTMODE},async,no_subtree_check,no_root_squash,no_all_squash)" ; \
+      echo "${BASEDIR}/home ${CLIENTIP}(rw,async,no_subtree_check)" ) \
+        > /etc/exports.d/${DESTNAME}.exports
+    ( echo "${ROOTDIR}/boot ${TFTPDIR}/boot none bind,ro 0 1" ; \
+      echo "${ROOTDIR}/home ${BASEDIR}/home none bind    0 1" ) \
+        > /etc/fstab.d/${DESTNAME}.fstab
+    ( echo "DEFAULT ${DESTNAME}" ; \
+      echo "LABEL ${DESTNAME}" ; \
+      echo "KERNEL vmlinuz" ; \
+      echo "APPEND ip=dhcp root=/dev/nfs nfsroot=${SERVERIP}:${ROOTDIR} ${ROOTMODE} initrd=initrd.img raid=noautodetect quiet splash ipv6.disable=1" \
+      ) > ${TFTPDIR}/pxelinux.cfg/default
+}
+
+OVERDIR=/usr/local/ovrfs
+
+# Add this line to /etc/fstab on the host:
+#
+# fstab_d /etc/fstab.d fuse nofail,defaults 0 0
+
+config_fstab_d () {
+    cat <<'EOF' > /usr/local/bin/fstab_d
+#!/bin/sh
+DIR="$1"
+[ -z "${DIR}" ] && exit 1
+/bin/mount -a -T ${DIR}
+EOF
+    chmod a+rx /usr/local/bin/fstab_d
+}
+
+config_overlay () {
+    # Source: https://github.com/hansrune/domoticz-contrib/blob/master/utils/mount_overlay
+    cat <<'EOF' | sed -e 's:<OVERDIR>:'${OVERDIR}':g' > ${ROOTDIR}/usr/local/bin/ovrfs
+#!/bin/sh
+DIR="$1"
+[ -z "${DIR}" ] && exit 1
+#
+# ro must be the first mount option for root .....
+#
+ROOT_MOUNT=$( awk '$2=="/" { print substr($4,1,2) }' /proc/mounts )
+
+if [ "$ROOT_MOUNT" = "ro" ] ; then
+    /bin/mount -t tmpfs tmpfs <OVERDIR>${DIR}
+    /bin/mkdir -p <OVERDIR>${DIR}/upper
+    /bin/mkdir -p <OVERDIR>${DIR}/work
+    OPTS="-o lowerdir=${DIR},upperdir=<OVERDIR>${DIR}/upper,workdir=<OVERDIR>${DIR}/work"
+    /bin/mount -t overlay ${OPTS} overlay ${DIR}
 fi
+
+if [ "${DIR}" = "/etc" ] ; then
+    # As soon as /etc is writable, fix hostname and nic:
+    setup_hostname () {
+	ipaddr=`hostname -I`
+	fqdn=`hostname -f`
+	hostname=`hostname`
+	echo ${hostname} > /etc/hostname
+	( echo "127.0.0.1	localhost" ; \
+	  echo "::1		localhost ip6-localhost ip6-loopback" ; \
+	  echo "ff02::1		ip6-allnodes" ; \
+	  echo "ff02::2		ip6-allrouters" ; \
+	  echo "${ipaddr} ${fqdn} ${hostname}" ; \
+	  ) > /etc/hosts
+    }
+    setup_nic () {
+	for nic in `ls /sys/class/net` ; do
+            ( echo "auto $nic"
+              if [ "$nic" != "lo" ] ; then
+		  echo "iface $nic inet dhcp"
+              else
+		  echo "iface $nic inet loopback"
+              fi
+            ) > /etc/network/interfaces.d/$nic
+	done
+    }
+
+    setup_hostname
+    setup_nic
+fi
+EOF
+    chmod a+rx ${ROOTDIR}/usr/local/bin/ovrfs
+}
+
+# vmdesktop () {
+    
+# }
+
+# vmserver () {
+# }
+
+# vmtiny () {
+# }
+
+# phdesktop () {
+# }
+
+# phserver () {
+# }
+
+case $1 in
+    wipeout)
+        wipeout
+        ;;
+    pxeserver)
+        pxeserver
+        ;;
+    do_chroot_pxe)
+        do_chroot_pxe
+        ;;
+    do_chroot)
+        do_chroot
+        ;;
+    bind_dirs)
+        settings_pxe
+        bind_dirs
+        ;;
+    unbind_dirs)
+        settings_pxe
+        unbind_dirs
+        ;;
+    config_chroot_pxe)
+        settings_pxe
+        bind_dirs
+        config_chroot do_chroot_pxe
+        unbind_dirs        
+        ;;
+    *)
+        echo "Usage: $0 all|pxeserver"
+        ;;
+esac
