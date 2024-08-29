cc550b998c34c9b62d5aa77b0e04290ec8cc37d7
Edison Mera <mera@process-design-center.com>
2024-08-29 14:25:12 +0200

    Improved replicate to show progress

--- backup/replicate.sh
+++ backup/replicate.sh
@@ -17,21 +17,30 @@ dryer () {
     fi
 }
 
+numelems () {
+    for i in $*; do echo $i; done|wc -l
+}
+
 replicate () {
     send_zfs=$1
     recv_zfs=$2
-
     send_dir=`zfs get -H mountpoint ${send_zfs} -o value`
     recv_dir=`zfs get -H mountpoint ${recv_zfs} -o value`
-    
     ssub_zfss="`zfs list -r -Ho name ${send_zfs}`"
+    snapshots="`zfs list -Ht snapshot -o name ${send_zfs} 2>/dev/null | sed -e "s:${send_zfs}@::g"`"
+    numsnaps=`numelems ${snapshots}`
+    numssubs=`numelems ${ssub_zfss}`
+    numcalls=$((${numsnaps}*${numssubs}))
+    curcall=0
 
-    for snapshot in `zfs list -Ht snapshot -o name ${send_zfs} 2>/dev/null | sed -e "s:${send_zfs}@::g"` ; do
+    for snapshot in ${snapshots} ; do
         changed=0
         for ssub_zfs in ${ssub_zfss} ; do
+	    curcall=$((${curcall}+1))
             ssub_dir="`zfs get -H mountpoint ${ssub_zfs} -o value | sed -e "s:${send_dir}::g"`"
             snapshot_dir=${send_dir}${ssub_dir}/.zfs/snapshot/${snapshot}
             if [ -d ${snapshot_dir} ] ; then
+		echo "# ${curcall}/${numcalls}: synchronizing ${send_dir}${ssubdir}@${snapshot}"
                 dryer rsync -a --delete ${snapshot_dir}/ ${recv_dir}/ || true
                 changed=1
             fi
